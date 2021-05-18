5f844450681efc3a18e68415f1e6efac91730970
Pacifique LINJANJA <pacilinja1@icloud.com>
2021-05-18 16:28:42 +0200

    Prepare the the header

---
+++ src/scripts/setAttributes.js
@@ -0,0 +1,9 @@
+const setAttributes = (elt, attrs) => {
+    const props = Object.keys(attrs);
+    props.forEach(prop => {
+      elt.setAttribute(prop, attrs[prop]);
+    });
+  };
+  
+  export default setAttributes;
+  
