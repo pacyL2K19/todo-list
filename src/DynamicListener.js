f3672a8013ebf5181fd8cf37bc068a8af0dee253
Pacifique LINJANJA <pacilinja1@icloud.com>
2021-05-20 00:20:18 +0200

    Update refactor

--- src/DynamicListener.js
+++ src/DynamicListener.js
@@ -1,41 +1,54 @@
-import { deleteProject, setActiveProject, deleteTask, updateTaskStatus } from './Logic'
-import { editTaskDom } from './DomFunctionality'
+/* eslint-disable func-names */
+/* eslint-disable import/no-cycle */
+import {
+  deleteProject, setActiveProject, deleteTask, updateTaskStatus,
+} from './Logic';
+import { editTaskDom } from './DomFunctionality';
 
 function addProjectListeners() {
-    const projectElements = document.querySelectorAll('.single-project-container h3')
-    const deleteProjectBtn = document.querySelectorAll('.delete-project-btn');
+  const projectElements = document.querySelectorAll('.single-project-container h3');
+  const deleteProjectBtn = document.querySelectorAll('.delete-project-btn');
 
-    projectElements.forEach((project) => { project.addEventListener('click', function() {
-        let index = this.parentNode.getAttribute('data-index');
-        setActiveProject(index);
-    })})
-    
-    deleteProjectBtn.forEach((project) => {project.addEventListener('click', function(){
-        let index = this.parentNode.getAttribute('data-index');
-        deleteProject(index)
-    })})
+  projectElements.forEach((project) => {
+    project.addEventListener('click', function () {
+      const index = this.parentNode.getAttribute('data-index');
+      setActiveProject(index);
+    });
+  });
+
+  deleteProjectBtn.forEach((project) => {
+    project.addEventListener('click', function () {
+      const index = this.parentNode.getAttribute('data-index');
+      deleteProject(index);
+    });
+  });
 }
-
 
 function addTasksListeners() {
-    const completeTaskBtn = document.querySelectorAll('.task-title-container');
-    const deleteTaskBtn = document.querySelectorAll('.delete-task');
-    const editTaskBtn = document.querySelectorAll('.edit-task');
+  const completeTaskBtn = document.querySelectorAll('.task-title-container');
+  const deleteTaskBtn = document.querySelectorAll('.delete-task');
+  const editTaskBtn = document.querySelectorAll('.edit-task');
 
-    deleteTaskBtn.forEach((project) => { project.addEventListener('click', function() {
-        let index = this.parentNode.getAttribute('data-index');
-        deleteTask(index);
-    })})
+  deleteTaskBtn.forEach((project) => {
+    project.addEventListener('click', function () {
+      const index = this.parentNode.getAttribute('data-index');
+      deleteTask(index);
+    });
+  });
 
-    editTaskBtn.forEach((project) => { project.addEventListener('click', function(){
-        let index = this.parentNode.getAttribute('data-index'); // grab the index of the clicked task
-        editTaskDom.show(index)
-    })})
+  editTaskBtn.forEach((project) => {
+    project.addEventListener('click', function () {
+      const index = this.parentNode.getAttribute('data-index'); // grab the index of the clicked task
+      editTaskDom.show(index);
+    });
+  });
 
-    completeTaskBtn.forEach((project) => { project.addEventListener('click', function(){
-        let index = this.parentNode.getAttribute('data-index');
-        updateTaskStatus(index)
-    })})
+  completeTaskBtn.forEach((project) => {
+    project.addEventListener('click', function () {
+      const index = this.parentNode.getAttribute('data-index');
+      updateTaskStatus(index);
+    });
+  });
 }
 
-export { addProjectListeners, addTasksListeners }
+export { addProjectListeners, addTasksListeners };
